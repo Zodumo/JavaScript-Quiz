de53b54352c315ac7ed7967a110aceb2ebeae300
Zodumo <zodumom@outlook.com>
2023-01-27 20:07:43 +0000

    started adding adding questions in object

---
+++ starter/assets/js/questions.js
@@ -0,0 +1,51 @@
+//Questions will be in objects
+
+
+// Are Java and javascript related?
+
+// What is javascript?
+
+// what is a JavaScript function?
+
+// which is a correct javascript external file? 
+
+var userChoices = [
+
+    {
+        question: "Are Java and JavaScript related?"
+        userOptions: ["Yes", "No"]
+        Answer: "No"
+
+    },
+
+    {
+        question: "What is JavaScript?"
+        userOptions: ["HyperText Markup Language is the most basic building block of the Web. It defines the meaning and structure of web content",
+                        "A simple mechanism for adding style (e.g., fonts, colors, spacing) to Web documents.",
+                        "a scripting language that enables you to create dynamically updating content, control multimedia, animate images, and pretty much everything else"
+                     ]
+        Answer: "a scripting language that enables you to create dynamically updating content, control multimedia, animate images, and pretty much everything else"
+
+    },
+
+    {
+        question: "what is a JavaScript function?"
+        userOptions: ["a special variable, which can hold more than one value",
+                      "A block of code designed to perform a particular task",
+                    "A thatloop repeats until a specified condition evaluates to false"]
+        Answer: ""
+
+    },
+    {
+        question: ""
+        userOptions: []
+        Answer: ""
+
+    },
+    {
+        question: ""
+        userOptions: []
+        Answer: ""
+
+    },
+]
