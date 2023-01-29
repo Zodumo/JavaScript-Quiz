1e2301ca3c0918ae55717a4771ec7bbf2e74b416
Zodumo <zodumom@outlook.com>
2023-01-29 22:46:38 +0000

    added all questions for the quiz

--- starter/assets/js/questions.js
+++ starter/assets/js/questions.js
@@ -21,9 +21,9 @@ var userChoices = [
     {
         question: "What is JavaScript?"
         userOptions: ["HyperText Markup Language is the most basic building block of the Web. It defines the meaning and structure of web content",
-                        "A simple mechanism for adding style (e.g., fonts, colors, spacing) to Web documents.",
-                        "a scripting language that enables you to create dynamically updating content, control multimedia, animate images, and pretty much everything else"
-                     ]
+            "A simple mechanism for adding style (e.g., fonts, colors, spacing) to Web documents.",
+            "a scripting language that enables you to create dynamically updating content, control multimedia, animate images, and pretty much everything else"
+        ]
         Answer: "a scripting language that enables you to create dynamically updating content, control multimedia, animate images, and pretty much everything else"
 
     },
@@ -31,21 +31,18 @@ var userChoices = [
     {
         question: "what is a JavaScript function?"
         userOptions: ["a special variable, which can hold more than one value",
-                      "A block of code designed to perform a particular task",
-                    "A thatloop repeats until a specified condition evaluates to false"]
+            "A block of code designed to perform a particular task",
+            "A thatloop repeats until a specified condition evaluates to false"]
         Answer: ""
 
     },
     {
-        question: ""
-        userOptions: []
-        Answer: ""
+        question: "When was Javacsript founded?"
+        userOptions: ["JavaScript was invented by James Brown in 1975",
+            "JavaScript was invented by Arsene Wenger in 2004",
+            "JavaScript was invented by Brendan Eich in 1995"]
+        Answer: "JavaScript was invented by Brendan Eich in 1995"
 
     },
-    {
-        question: ""
-        userOptions: []
-        Answer: ""
 
-    },
 ]
