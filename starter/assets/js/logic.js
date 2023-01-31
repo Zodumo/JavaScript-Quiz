bd540fd869706d6ad6fee3600f07d599dfdc1120
Zodumo <zodumom@outlook.com>
2023-01-31 01:01:44 +0000

    added initial timer logic

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -33,6 +33,19 @@ var questionsEL = document.query("#quizquestion");
 var userOptionsEL = document.query("#userOptions")
 
 
+
 //sfx files. Elements to using Audio() constructor to add sound to the game for correct and incorrect answer
 var correctSfx = new Audio("assets/sfx/correct.wav");
 var incorrectSfx = new Audio("assets/sfx/incorrect.wav");
+
+//start of logic for the timer
+var interval;
+var time = document.querySelector("#time");
+
+//Allocating 60 seconds for the duration of the quiz
+var startTime = 60;
+// Indexes of an array start at 0 so this question number starts at -1 so no questions are displayed until the player starts the quiz
+var questionNumber = -1;
+
+//score for the player is set at 0. 
+var score = 0;
